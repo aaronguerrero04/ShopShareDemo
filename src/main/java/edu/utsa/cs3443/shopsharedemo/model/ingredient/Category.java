400aff6942bd30934d6b64dd986f7da734ad66bb
AlexCaldwell27 <acaldwell2524@gmail.com>
2025-10-27 18:05:06 -0500

    Create Category and MealType

---
+++ src/main/java/edu/utsa/cs3443/shopsharedemo/model/ingredient/Category.java
@@ -0,0 +1,11 @@
+package main.java.edu.utsa.cs3443.shopsharedemo.model.ingredient;
+
+public enum Category {
+    MEAT,
+    FRUIT,
+    GRAINS,
+    VEGETABLE,
+    BEVERAGES,
+    SNACKS,
+    DAIRY,
+}
---
+++ src/main/java/edu/utsa/cs3443/shopsharedemo/model/ingredient/MealType.java
@@ -0,0 +1,8 @@
+package main.java.edu.utsa.cs3443.shopsharedemo.model.ingredient;
+
+public enum MealType {
+    LUNCH,
+    BREAKFAST,
+    SNACK,
+    DINNER;
+}
