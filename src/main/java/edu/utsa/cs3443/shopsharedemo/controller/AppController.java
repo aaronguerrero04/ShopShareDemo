37ae6fe18ef576e4b51f5ae3501fc9ed40cce036
gabriel-at <gabrielalejtabar@gmail.com>
2025-10-27 18:56:29 -0500

    Added AppController.java
    Needed a controller class to initialize objects outside of the model, view, and main classes. Main.java uses this to launch main menu right now.

---
+++ src/main/java/edu/utsa/cs3443/shopsharedemo/controller/AppController.java
@@ -0,0 +1,32 @@
+package controller;
+
+import model.GroceryList;
+import model.Pantry;
+import view.GroceryListScreen;
+import view.PantryScreen;
+import view.mainmenu.MainMenuScreen;
+
+public class AppController { // needed a class to initialize models & views outside of main/view classes.
+    private Pantry pantry; // pantry model
+    private GroceryList groceryList; // list model
+    private IngredientController ingredientController; // handle ingredient operations
+
+    public AppController() {
+        groceryList = new GroceryList();
+        pantry = new Pantry(groceryList);
+        /* initialize a grocery list, then initialize a pantry with the grocery list
+            this ensures ingredients removed from the pantry are added to the grocery list
+         */
+
+        ingredientController = new IngredientController(pantry, groceryList); // initialize ingredient controller
+
+        // initialize respective view classes
+        PantryScreen.initialize(pantry);
+        GroceryListScreen.initialize(groceryList, pantry);
+    }
+
+    public void startApplication() {
+        //start menu
+        MainMenuScreen.displayMainMenu();
+    }
+}
