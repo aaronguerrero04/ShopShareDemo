6e2aa9b93d4af0c1790345111d67d087a07da9d3
Aaron Guerrero <aaronguerrero500@gmail.com>
2025-10-27 11:38:25 -0500

    updated Main.java to display Main Menu

---
+++ src/main/java/edu/utsa/cs3443/shopsharedemo/Main.java
@@ -0,0 +1,11 @@
+package main.java.edu.utsa.cs3443.shopsharedemo;
+
+import main.java.edu.utsa.cs3443.shopsharedemo.view.mainmenu.MainMenuScreen;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        MainMenuScreen.displayMainMenu();
+    }
+}
