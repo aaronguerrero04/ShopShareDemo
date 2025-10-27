7d82a3ea7c11c10be210c14b85107f1331caa06c
gabriel-at <gabrielalejtabar@gmail.com>
2025-10-27 18:57:43 -0500

    Added IngredientController.java

---
+++ src/main/java/edu/utsa/cs3443/shopsharedemo/controller/IngredientController.java
@@ -0,0 +1,38 @@
+package controller;
+
+import model.GroceryList;
+import model.ingredient.Ingredient;
+import model.Pantry;
+
+import java.util.List;
+
+public class IngredientController {
+    private Pantry pantry;
+    private GroceryList groceryList;
+
+    public IngredientController(Pantry pantry, GroceryList groceryList) {
+        this.pantry = pantry;
+        this.groceryList = groceryList;
+    }
+
+    public void addIngredientToPantry(Ingredient ingredient) { // self-explanatory
+        pantry.addIngredient(ingredient);
+    }
+
+    public void removeIngredientFromPantry(Ingredient ingredient) { // self-explanatory
+        pantry.removeIngredient(ingredient);
+    }
+
+    public void moveIngredientToPantry(Ingredient ingredient) { // self-explanatory
+        groceryList.removeIngredient(ingredient);
+        pantry.addIngredient(ingredient);
+    }
+
+    public List<Ingredient> getPantryIngredients() { // self-explanatory
+        return pantry.getIngredients();
+    }
+
+    public List<Ingredient> getGroceryListIngredients() { // self-explanatory
+        return groceryList.getIngredients();
+    }
+}
