29a1efbf45a98bee36c9ea76a7815d17df1740be
Aaron Guerrero <aaronguerrero500@gmail.com>
2025-10-27 11:34:29 -0500

    added getters and setters to Ingredient.java

--- src/main/java/edu/utsa/cs3443/shopsharedemo/model/Ingredient.java
+++ src/main/java/edu/utsa/cs3443/shopsharedemo/model/Ingredient.java
@@ -5,4 +5,41 @@ public class Ingredient
     private String name;
     private double price;
     private boolean isInGroceryList;
+
+    public Ingredient(String name, double price, boolean isInGroceryList)
+    {
+        this.name = name;
+        this.price = price;
+        this.isInGroceryList = isInGroceryList;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    public boolean isInGroceryList()
+    {
+        return isInGroceryList;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setPrice(double price)
+    {
+        this.price = price;
+    }
+
+    public void setInGroceryList(boolean inGroceryList)
+    {
+        isInGroceryList = inGroceryList;
+    }
 }
