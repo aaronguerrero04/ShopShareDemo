7654f4152f785415f84155ceee76acf0cc812063
AlexCaldwell27 <acaldwell2524@gmail.com>
2025-10-27 19:58:39 -0500

    add recipe loaded

--- src/main/java/edu/utsa/cs3443/shopsharedemo/controller/AppController.java
+++ src/main/java/edu/utsa/cs3443/shopsharedemo/controller/AppController.java
@@ -3,18 +3,27 @@ package main.java.edu.utsa.cs3443.shopsharedemo.controller;
 
 import main.java.edu.utsa.cs3443.shopsharedemo.model.GroceryList;
 import main.java.edu.utsa.cs3443.shopsharedemo.model.Pantry;
+import main.java.edu.utsa.cs3443.shopsharedemo.model.recipe.Recipe;
+import main.java.edu.utsa.cs3443.shopsharedemo.model.recipe.RecipeRepository;
 import main.java.edu.utsa.cs3443.shopsharedemo.view.GroceryListScreen;
 import main.java.edu.utsa.cs3443.shopsharedemo.view.PantryScreen;
 import main.java.edu.utsa.cs3443.shopsharedemo.view.mainmenu.MainMenuScreen;
+
+import java.util.ArrayList;
 
 public class AppController { // needed a class to initialize models & views outside of main/view classes.
     private Pantry pantry; // pantry model
     private GroceryList groceryList; // list model
     private IngredientController ingredientController; // handle ingredient operations
+    private ArrayList<Recipe> recipes;
+    private RecipeRepository recipeRepository;
 
     public AppController() {
         groceryList = new GroceryList();
         pantry = new Pantry(groceryList);
+        recipeRepository = new RecipeRepository();
+        recipes = new ArrayList<Recipe>(recipeRepository.loadRecipesFromFile("data/daily_food_nutrition_dataset.csv", "data/recipes.txt"));
+
         /* initialize a grocery list, then initialize a pantry with the grocery list
             this ensures ingredients removed from the pantry are added to the grocery list
          */
@@ -24,6 +33,7 @@ public class AppController { // needed a class to initialize models & views outs
         // initialize respective view classes
         PantryScreen.initialize(pantry);
         GroceryListScreen.initialize(groceryList, pantry);
+
     }
 
     public void startApplication() {
--- src/main/java/edu/utsa/cs3443/shopsharedemo/model/planner/Planner.java
+++ src/main/java/edu/utsa/cs3443/shopsharedemo/model/planner/Planner.java
@@ -1,7 +1,40 @@
 package main.java.edu.utsa.cs3443.shopsharedemo.model.planner;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Planner {
     ArrayList<PlannerDay> plannerDays;
+
+    public void loadPlannerFromFile(String filePath){
+        try {
+            File plannerFile = new File(filePath);
+            Scanner plannerReader = new Scanner(plannerFile);
+
+            while (plannerReader.hasNextLine()){
+                String date = plannerReader.nextLine();
+                String breakfastCode = plannerReader.nextLine();
+                String lunchCode = plannerReader.nextLine();
+                String dinnerCode = plannerReader.nextLine();
+
+
+
+                String[] splitDate = date.split("/");
+                short month = Short.parseShort(splitDate[0]);
+                short day = Short.parseShort(splitDate[1]);
+                int year = Integer.parseInt(splitDate[2]);
+
+                PlannerDay pd = new PlannerDay(new Date(month, day, year), );
+            }
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+    public Planner(){
+        plannerDays = new ArrayList<PlannerDay>();
+        loadPlannerFromFile("data/saved_planner.txt");
+    }
 }
