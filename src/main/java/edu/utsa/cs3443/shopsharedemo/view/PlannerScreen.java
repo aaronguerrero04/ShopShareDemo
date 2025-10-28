53d1c83043a3a961ed8c61f44fd16b336032be90
AlexCaldwell27 <acaldwell2524@gmail.com>
2025-10-27 19:14:46 -0500

    Update PlannerScreen.java

--- src/main/java/edu/utsa/cs3443/shopsharedemo/view/PlannerScreen.java
+++ src/main/java/edu/utsa/cs3443/shopsharedemo/view/PlannerScreen.java
@@ -4,6 +4,13 @@ public class PlannerScreen
 {
     public static void displayPlanner()
     {
-        System.out.println("Planner Screen");
+        System.out.println("============================================");
+        System.out.println("|\t\t\t\t Planner \t\t\t\t   |");
+        System.out.println("============================================");
+        System.out.println("\t[1] View Planner");
+        System.out.println("\t[2] Add Meal");
+        System.out.println("\t[3] Remove Meal");
+        System.out.println("\t[4] ");
+
     }
 }
